5ee40d4ae09ea5917bff3086a3dde877e0d85d3c
season <sionwer4@naver.com>
2023-12-31 17:27:52 +0900

    feat: Add service logic to controll the quantity of item in the item entity class

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,4 +22,20 @@ public abstract class Item {
     private int stockQuantity;
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    // 비즈니스 로직
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    // setter를 가지고 바깥에서 변경해서 넣는 것이 아니라
+    // 이렇게 도메인 모델 안에서 값 변경 로직을 작성해준다. 
+    // 이것이 가장 객체 지향적인 방식
+    public void removeStock(int quentity){
+        int restStock = this.stockQuantity - quentity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
