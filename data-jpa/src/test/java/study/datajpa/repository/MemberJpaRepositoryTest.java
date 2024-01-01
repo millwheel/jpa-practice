57da806ba334f859735e6d7c414872472b837f9a
season <sionwer4@naver.com>
2024-01-01 21:49:43 +0900

    feat: Create member and related repository

---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,28 @@
+package study.datajpa.repository;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+public class MemberJpaRepositoryTest {
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //JPA 􀧂􀱭􀱭 􀘲􀩌􀢿 􀠁􀩢
+    }
+}
