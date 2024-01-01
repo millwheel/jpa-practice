749cbeb11b2cfa98362baf33fa69c3f3ae49ecf5
season <sionwer4@naver.com>
2024-01-01 20:55:46 +0900

    feat: Set the default batch fetch size of hibernate

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,6 +31,16 @@ public class OrderApiController {
     public List<OrderDto> ordersV3() {
         // 쿼리 한 방에 나감
         List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -61,7 +61,7 @@ public class OrderRepository {
         // 성능 최적화
         // fetch join <- jpa에만 있는 문법
         // 이것을 사용하면 proxy 객체도 아니고 실제 Java 객체에 값을 다 채워서 가져 온다.
-        // fetch를 사용하므로 지연로딩 자체가 일어나지 않는다.
+        // fetch를 사용하므로 지연로딩 자체가 일어나지 않고 바로 가져온다.
         return em.createQuery(
                         "select o from Order o" +
                                 " join fetch o.member m" +
@@ -77,6 +77,21 @@ public class OrderRepository {
                                 " join fetch o.delivery d" +
                                 " join fetch o.orderItems oi" +
                                 " join fetch oi.item i", Order.class)
+                .setFirstResult(1) // out of memory 뜰 수도 있음.
+                .setMaxResults(100) // 이렇게 작성해도 DB에서 10000개 가져온 후, 메모리에서 100개 페이징 처리하게 된다.
+                .getResultList(); // fetch join을 하는 순간 order의 기준이 모두 틀어지게 됨. 1대다 fetch join 할 때는 페이징 못 씀
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        // ToOne에만 fetch join을 사용한다. 이건 하나 씩만 연결하기 때문에 성능에 영향 안 준다.
+        // 컬렉션에는 fetch join을 사용하지 않는다. 컬렉션은 지연 로딩으로 조회한다.
+        // 대신 지연 로딩 성능 최적화를 위해 @BatchSize를 적용한다.
+        return em.createQuery(
+                        "select o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
                 .getResultList();
     }
 }
