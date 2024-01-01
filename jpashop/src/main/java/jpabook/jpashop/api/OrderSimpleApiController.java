cbfbe332e6d2eca496b64e0dddff9c4e7947e969
season <sionwer4@naver.com>
2024-01-01 13:43:42 +0900

    feat: Create simple order dto

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.dto.SimpleOrderDto;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +22,18 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
-            order.getMember().getName(); //Lazy 􀑊􀪁 􀭡􀓝􀴚
-            order.getDelivery().getAddress(); //Lazy 􀑊􀪁 􀭡􀓝􀴜
+            order.getMember().getName(); // getName을 하게 되면 실제 객체 필요. LAZY가 강제 초기화가 된다. member까지 끌고 온다.
+            order.getDelivery().getAddress(); // 마찬가지로 Address를 끌고 온다. LAZY 강체 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+        return result;
     }
 }
