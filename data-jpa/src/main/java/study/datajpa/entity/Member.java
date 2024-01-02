71f4a76522ec9a2248e28508a0c80f53040aff1b
박시준 <sjpark@logblack.com>
2024-01-02 17:23:22 +0900

    feat: Create team entity

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,29 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
     @Id
     @GeneratedValue
     private Long id;
     private String username;
-
-    protected Member(){
-
-    }
+    private int age;
+    @ManyToOne
+    @JoinColumn
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ data-jpa/src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,25 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+
+
+}
+
