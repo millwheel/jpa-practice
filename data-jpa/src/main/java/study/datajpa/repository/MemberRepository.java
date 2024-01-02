e7f235b126996cf4609965539597a13c7bf57b62
박시준 <sjpark@logblack.com>
2024-01-02 18:59:56 +0900

    feat: Add query to read value and dto from db

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,9 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -10,4 +13,17 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // 쿼리 메서드
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // 쿼리 정의하기, 문법은 JPQL
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    // 쿼리로 값만 조회
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // 쿼리로 DTO로 가져오기
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -15,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+
     @Test
     public void testMember() {
         Member member = new Member("memberA");
@@ -24,5 +27,16 @@ public class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
     }
 }
