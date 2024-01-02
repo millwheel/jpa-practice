bce987096148a86ca20a3fcd8d7d98a33c8488f8
박시준 <sjpark@logblack.com>
2024-01-02 18:27:16 +0900

    feat: Add query method

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -3,7 +3,7 @@ package study.datajpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Boot 사용하면 하위 디렉토리에서 Spring Data Jpa 사용 가능
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,11 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+// Spring Data JPA가 구현 클래스를 알아서 프록시로 자동으로 만들어서 붙여줌
+// @Repository 생략 가능.
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 쿼리 메서드
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
