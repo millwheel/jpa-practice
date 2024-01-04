257ac0fa9e7b6b8b4eae435956d140e285e4d7e6
박시준 <sjpark@logblack.com>
2024-01-04 16:46:02 +0900

    feat: querydsl 동작 완료 확인

---
+++ querydsl/src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,14 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
