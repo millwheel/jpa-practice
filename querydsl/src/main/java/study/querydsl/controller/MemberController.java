1e2c8c809d293c4714da866e288d5d6326585cd4
season <sionwer4@naver.com>
2024-01-05 12:24:55 +0900

    feat: Add pagination to call member with condition

--- querydsl/src/main/java/study/querydsl/controller/MemberController.java
+++ querydsl/src/main/java/study/querydsl/controller/MemberController.java
@@ -1,11 +1,15 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -13,8 +17,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
     }
+
+    @GetMapping("/v2/members")
+    public List<MemberTeamDto> searchMemberV2(MemberSearchCondition condition) {
+        return memberRepository.search(condition);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberWithPage(@RequestParam(defaultValue = "0") int page,
+                                                    @RequestParam(defaultValue = "10") int size,
+                                                    MemberSearchCondition condition) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return memberRepository.searchPageSimple(condition, pageRequest);
+    }
+
 }
